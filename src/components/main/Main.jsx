016d84561245d45e8222e4af192af6e2bfabb221
EmelyanovVladimir <emelyanov.v.s9999@gmail.com>
2023-10-19 23:07:15 +0300

    Main update

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,52 +3,32 @@ import '../../normalize.css'
 import '../../reset.css'
 import './main.css'
 import search from '../../img/icons/Search.png'
+import DartVader from '../../img/charecters/Dart Vader.png'
 
 const Main = () => {
     return (
-        <main className='mainClass'>
-            <div className="content">
-                <div className="divInput">
-                    <h2 className='divInput_text'>Characters</h2>
-                    <div className="divInput_search">
-                        <button><img src = {search} alt="search_icon" /></button>
-                        <input type="text" placeholder='Search character...' />
-                    </div>
+        <main>
+            <div class="namePlusInput">
+                <h2>Character</h2>
+                <div class="namePlusInput__divInput">
+                    <img src= {search} alt="search icon"/>
+                    <input type="text" placeholder="Search character..."/>
                 </div>
-                <div className="divOfCards">
-                    <div className="charactersCards">
-                        <div className="info">
-                            <div className="info_name">
-                                <h2></h2>
-                                <button>Learn more</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="charactersCards">
-                        <div className="info">
-                            <div className="info_name">
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className="charactersCards">
-                        <div className="info">
-                            <div className="info_name">
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className="charactersCards">
-                        <div className="info">
-                            <div className="info_name">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
             </div>
-        </main>
+            <div class="characterInfo">
+                <div class="characterInfo__card">
+                    <div class="characterInfo__info">
+                        <div class="characterInfo__smallBlock">
+                            <h3>Dart Vader</h3>
+                            <button>Learn more</button>
+                        </div>
+                    </div>
+                    <div class="characterInfo__photo">
+                        <img src= {DartVader} alt="Dart Vader"/>
+                    </div>
+                </div>
+            </div>
+    </main>
     )
 }
 
